5de8e1f3e716401e19210c625bb15870fbddd7ca
e13q <110967581+e13q@users.noreply.github.com>
2024-10-20 23:07:50 +0300

    replace urlllib with requests, fix naming, fix image saving

--- project/places/management/commands/load_place.py
+++ project/places/management/commands/load_place.py
@@ -1,8 +1,8 @@
-import json
 import os
-from urllib.request import urlopen
+import requests
 
 from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
 
 from places.models import Place, Image
 
@@ -16,43 +16,43 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         json_url = kwargs['json_url']
         try:
-            with urlopen(json_url) as file:
-                data = json.load(file)
-                place, created = Place.objects.get_or_create(
-                    title=data['title'],
-                    defaults={
-                        'short_description': data['description_short'],
-                        'long_description': data['description_long'],
-                        'longitude': data['coordinates']['lng'],
-                        'latitude': data['coordinates']['lat'],
-                    }
+            raw_place = requests.get(json_url)
+            raw_place.raise_for_status()
+            raw_place = raw_place.json()
+            place, created = Place.objects.get_or_create(
+                title=raw_place['title'],
+                defaults={
+                    'short_description': raw_place['description_short'],
+                    'long_description': raw_place['description_long'],
+                    'longitude': raw_place['coordinates']['lng'],
+                    'latitude': raw_place['coordinates']['lat'],
+                }
+            )
+            if created:
+                self.stdout.write(
+                    self.style.SUCCESS(f"Place '{place.title}' created")
                 )
-                if created:
-                    self.stdout.write(
-                        self.style.SUCCESS(f"Place '{place.title}' created")
+                for index, img_url in enumerate(raw_place['imgs']):
+                    raw_img = requests.get(img_url)
+                    raw_img.raise_for_status()
+                    img_content = ContentFile(
+                        raw_img.content, name=os.path.basename(img_url)
                     )
-                    for index, img_url in enumerate(data['imgs']):
-                        img_temp = urlopen(img_url)
-                        img, _ = Image.objects.get_or_create(
-                            place=place, ordinal_number=index + 1,
-                            defaults={
-                                'ordinal_number': index + 1,
-                                'place': place,
-                            }
-                        )
-                        img.image.save(
-                            os.path.basename(img_url),
-                            img_temp,
-                            save=True
-                        )
-                        img.save()
-                    self.stdout.write(self.style.SUCCESS(
-                        f"Images added to place '{place.title}'"
-                    ))
-                else:
-                    self.stdout.write(self.style.WARNING(
-                        f"Place '{place.title}' already exists"
-                    ))
-                    return None
+                    Image.objects.get_or_create(
+                        place=place, ordinal_number=index + 1,
+                        defaults={
+                            'ordinal_number': index + 1,
+                            'place': place,
+                            'image': img_content
+                        }
+                    )
+                self.stdout.write(self.style.SUCCESS(
+                    f"Images added to place '{place.title}'"
+                ))
+            else:
+                self.stdout.write(self.style.WARNING(
+                    f"Place '{place.title}' already exists"
+                ))
+                return None
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"Error: {str(e)}"))
