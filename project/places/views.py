341e4f2e35ca2504d805d1626392b1ba5577d6e6
e13q <110967581+e13q@users.noreply.github.com>
2024-10-20 17:07:44 +0300

    remove non reusable funcs

--- project/places/views.py
+++ project/places/views.py
@@ -5,48 +5,29 @@ from django.urls import reverse
 from places.models import Place
 
 
-def serialize_places(places: list):
-    return {
-        "type": "FeatureCollection",
-        "features": places
-    }
-
-
-def serialize_place(place: Place):
-    return {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [place.longitude, place.latitude]
-        },
-        "properties": {
-            "title": place.title,
-            "placeId": place.id,
-            "detailsUrl": reverse('place_detail', args=[place.id])
-        }
-    }
-
-
-def serialize_place_json(place: Place):
-    return {
-        "title": place.title,
-        "imgs": [object.image.url for object in place.images.all()],
-        "description_short": place.short_description,
-        "description_long": place.long_description,
-        "coordinates": {
-            "lat": place.latitude,
-            "lng": place.longitude
-        }
-    }
-
-
 def show_index(request):
     places = []
     db_places = Place.objects.all()
     for place in db_places:
-        places.append(serialize_place(place))
+        places.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.longitude, place.latitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": reverse('place_detail', args=[place.id])
+                }
+            }
+        )
     context = {
-        "geojson": serialize_places(places)
+        "geojson": {
+            "type": "FeatureCollection",
+            "features": places
+        }
     }
     return render(request, 'index.html', context)
 
@@ -55,4 +36,15 @@ def get_place(request, place_id):
     place = get_object_or_404(
         Place.objects.prefetch_related('images'), id=place_id
     )
-    return JsonResponse(serialize_place_json(place))
+    return JsonResponse(
+        {
+            "title": place.title,
+            "imgs": [object.image.url for object in place.images.all()],
+            "description_short": place.short_description,
+            "description_long": place.long_description,
+            "coordinates": {
+                "lat": place.latitude,
+                "lng": place.longitude
+            }
+        }
+    )
